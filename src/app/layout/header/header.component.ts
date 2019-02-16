635a3bb5989a1faaa74333aef9dd4020d0f44738
Andrew Omelchenko <andrewom43@gmail.com>
2019-02-17 00:08:03 +0200

    Fix navbar doesn't collapse issue

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  isCollapsed = true;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleCollapse(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
