0d18a28ad917a3c4b27d7334cfe8c12656b49e49
Andrew Omelchenko <andrewom43@gmail.com>
2019-02-16 20:53:43 +0200

    Add basic shared components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,27 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductComponent } from './pages/product/product.component';
+import { MyCartComponent } from './pages/my-cart/my-cart.component';
+import { FeaturedProductCardComponent } from './shared/featured-product-card/featured-product-card.component';
+import { ProductDetailsCardComponent } from './shared/product-details-card/product-details-card.component';
+import { ProductDetailsItemComponent } from './shared/product-details-item/product-details-item.component';
+import { BannerCardComponent } from './shared/banner-card/banner-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ProductsComponent,
+    ProductComponent,
+    MyCartComponent,
+    FeaturedProductCardComponent,
+    ProductDetailsCardComponent,
+    ProductDetailsItemComponent,
+    BannerCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/product-details-item/product-details-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details-item',
+  templateUrl: './product-details-item.component.html',
+  styleUrls: ['./product-details-item.component.scss']
+})
+export class ProductDetailsItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
