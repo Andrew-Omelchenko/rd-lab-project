786c96516f8f841e0b46a83621e63b136fdfe2ea
Andrew Omelchenko <andrewom43@gmail.com>
2019-02-16 20:58:17 +0200

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {HomeComponent} from './pages/home/home.component';
+import {ProductsComponent} from './pages/products/products.component';
+import {ProductComponent} from './pages/product/product.component';
+import {MyCartComponent} from './pages/my-cart/my-cart.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: '/products', component: ProductsComponent },
+  { path: '/product/:id', component: ProductComponent },
+  { path: '/my-cart', component: MyCartComponent }
 ];
 
 @NgModule({
